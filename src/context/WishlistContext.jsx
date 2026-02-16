e65493e00f4da6f6cae9c4f27f43ab59db5b3483
adyaananyamishra <adyaananyamishra@email.com>
2026-02-16 21:22:02 +0530

    chore:updated wishlist page

--- src/context/WishlistContext.jsx
+++ src/context/WishlistContext.jsx
@@ -1,67 +1,10 @@
-<<<<<<< HEAD
 import { createContext, useContext, useEffect, useState } from "react";
 
 const WishlistContext = createContext();
-=======
-// import { createContext, useContext, useEffect, useState } from "react";
-
-// const WishlistContext = createContext();
-
-// export const WishlistProvider = ({ children }) => {
-//   const [wishlist, setWishlist] = useState([]);
-
-//   // Load wishlist from localStorage
-//   useEffect(() => {
-//     const stored = localStorage.getItem("wishlist");
-//     if (stored) {
-//       setWishlist(JSON.parse(stored));
-//     }
-//   }, []);
-
-//   // Save wishlist to localStorage
-//   useEffect(() => {
-//     localStorage.setItem("wishlist", JSON.stringify(wishlist));
-//   }, [wishlist]);
-
-//   const addToWishlist = (product) => {
-//     setWishlist((prev) => {
-//       if (prev.find((p) => p.id === product.id)) return prev;
-//       return [...prev, product];
-//     });
-//   };
-
-//   const removeFromWishlist = (id) => {
-//     setWishlist((prev) => prev.filter((item) => item.id !== id));
-//   };
-
-//   const clearWishlist = () => setWishlist([]);
-
-//   return (
-//     <WishlistContext.Provider
-//       value={{ wishlist, addToWishlist, removeFromWishlist, clearWishlist }}
-//     >
-//       {children}
-//     </WishlistContext.Provider>
-//   );
-// };
-
-// export const useWishlist = () => {
-//   const context = useContext(WishlistContext);
-//   if (!context) {
-//     throw new Error("useWishlist must be used inside WishlistProvider");
-//   }
-//   return context;
-// };
-
-import { createContext, useContext, useEffect, useState } from "react";
-
-export const WishlistContext = createContext();
->>>>>>> c2b1d74 (chore:updated admin panel)
 
 export const WishlistProvider = ({ children }) => {
   const [wishlist, setWishlist] = useState([]);
 
-<<<<<<< HEAD
   // Load wishlist from localStorage
   useEffect(() => {
     const stored = localStorage.getItem("wishlist");
@@ -71,13 +14,6 @@ export const WishlistProvider = ({ children }) => {
   }, []);
 
   // Save wishlist to localStorage
-=======
-  useEffect(() => {
-    const stored = localStorage.getItem("wishlist");
-    if (stored) setWishlist(JSON.parse(stored));
-  }, []);
-
->>>>>>> c2b1d74 (chore:updated admin panel)
   useEffect(() => {
     localStorage.setItem("wishlist", JSON.stringify(wishlist));
   }, [wishlist]);
